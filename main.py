89aa19901d8645bfcf5d5f32d943edb5d4ef9d37
ayo dev <yly@protonmail.com>
2022-07-11 13:13:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
-# ">>>post button"  <-to post the verification button (no quotes)
-
 import discord
-from discord.ui import Button, View
 from discord.ext import commands, tasks
 
-role_id = 1234  # role of verified users
-channel_id = 1234  # should be the verification channel id
 keywordbanlist = ["captcha", "scammerbot"]  # keyword list of banned words in usernames
+role_id = 123  # role id of verified users
+channel_id = 123  # verification channel id
+token = "xxx" # from the discord developer panel -> bot section 
 
-# token found in the discord developer portal -> application -> bots
-token = '1234asdf'
+class PersistentView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
 
+    @discord.ui.button(label="Verify", style=discord.ButtonStyle.green, custom_id="persistent_view:verify")
+    async def green(self, button: discord.ui.Button, interaction: discord.Interaction):
+        message = interaction.message
+        member = message.guild.get_member(interaction.user.id)
+        role = message.guild.get_role(role_id)
+        baddy = False
+        for items in keywordbanlist:
+            if items in member.name.lower():
+                await member.ban(reason="baddy")
+                baddy = True
+        if baddy == False:
+            await interaction.response.send_message("Verified!", ephemeral=True)
+            await member.add_roles(role)
+            await interaction.response.defer()
+        else:
+            print("baddy detected")
 
-intents = discord.Intents().all()
-bot = commands.Bot(command_prefix=">>>", intents=intents)
+class PersistentViewBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents().all()
+        intents.message_content = True
+        super().__init__(command_prefix=commands.when_mentioned_or(">>>"), intents=intents)
+        self.persistent_views_added = False
 
+    async def on_ready(self):
+        if not self.persistent_views_added:
+            self.add_view(PersistentView())
+            self.persistent_views_added = True
+        if not usercheck.is_running():
+            usercheck.start()
 
-@bot.event
-async def on_ready():
-    print('Logged in as ' + str(bot.user))
-    if not usercheck.is_running():
-        usercheck.start()
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
 
+bot = PersistentViewBot()
+
+@bot.command()
+async def postbutton(ctx: commands.Context):
+    await ctx.send("Verify that you are (probably) human.", view=PersistentView())
 
 @bot.event
 async def on_member_join(member):
@@ -30,40 +56,14 @@ async def on_member_join(member):
             print("banning: " + member.name)
             await member.ban(reason="bot")
 
-
-@bot.command()
-async def postbutton(ctx):
-    button = Button(label="Verify", style=discord.ButtonStyle.green, emoji="✔️")
-
-    async def button_callback(interaction):
-        message = interaction.message
-        member = message.guild.get_member(interaction.user.id)
-        role = message.guild.get_role(role_id)
-        if "captcha" in member.name.lower():
-            print("banning: " + member.name)
-            await member.ban(reason="bot")
-        try:
-            await member.add_roles(role)
-            await interaction.response.defer()
-            await interaction.followup.send("Verified!", ephemeral=True)
-        except:
-            pass
-
-    button.callback = button_callback
-
-    view = View()
-    view.add_item(button)
-    await ctx.send("Click below to prove you are (probably) human.", view=view)
-
-
 @tasks.loop(seconds=10)
 async def usercheck():
     channel = bot.get_channel(channel_id)
     users = channel.members
     for members in users:
-        if "captcha" in members.name.lower():
-            print("banning: " + members.name)
-            await members.ban(reason="bot")
-
+        for items in keywordbanlist:
+            if items in members.name.lower():
+                print("banning: " + members.name)
+                await members.ban(reason="bot")
 
 bot.run(token)
